64ee5eedb074978eceb6ef431b31752be9409f64
Walidghidhaoui <walidghidhawi@gmail.com>
2026-08-04 00:09:04 +0100

    updte

--- src/App.js
+++ src/App.js
@@ -12,7 +12,24 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
+       {/* Navbar */}
+      <nav className="navbar">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+        </div>
+        <ul className={`nav-links ${isMenuOpen ? 'show' : ''}`}>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div className="hamburger" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
 
 
       {/* Hero */}
